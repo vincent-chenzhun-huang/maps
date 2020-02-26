c7f39ef25edbd6675ea62d4cba1ffc34e21be48d
Vincent Huang <39947361+VincentCloud@users.noreply.github.com>
2020-02-26 15:59:10 -0500

    Initial Support for Google Maps

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { User } from './User';
-import { Company } from './Company'
+import { Company } from './Company';
 
-const user = new User()
-console.log(user)
-
-const company = new Company()
-console.log(company)
+new google.maps.Map(document.getElementById("map") as HTMLDivElement, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+}
+);
